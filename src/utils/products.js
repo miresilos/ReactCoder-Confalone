eb3c35c05b96888d610a6288c722522149e71c0b
Miresilos <57138575+miresilos@users.noreply.github.com>
2022-05-26 01:05:55 -0300

    Promesas

---
+++ src/utils/products.js
@@ -0,0 +1,87 @@
+import React from 'react'
+const products = [
+    {
+        id:30,
+        name: "Vodka",
+        stock: 5,
+        cost: 5000,
+        description: "Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur aliquet quam id dui posuere blandit.",
+        image: [
+            "https://thumbs.dreamstime.com/b/poznan-poland-mar-bottles-several-global-brands-vodka-world%C3%A2%E2%82%AC%E2%84%A2s-largest-internationally-traded-spirit-estimated-sale-116547157.jpg"
+        ],
+        sales: 0,
+        categoryId: 1,
+        category: {
+            name: "Destilados",
+            id: 1
+        }
+    },
+    {
+        id:31,
+        name: "Ron",
+        stock: 10,
+        cost: 6000,
+        description: "Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur aliquet quam id dui posuere blandit.",
+        image: [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ33AWv0N_47I6vdAgWfOgGiTSbCRUpNd1fL_KZXMNVnpnwflJtxJ6SfV81y8DWWiovsqA&usqp=CAU"
+        ],
+        sales: 0,
+        categoryId: 1,
+        category: {
+            name: "Destilados",
+            id: 1
+        }
+    },
+    {
+        id:32,
+        name: "Gin",
+        stock: 4,
+        cost: 7000,
+        description: "Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur aliquet quam id dui posuere blandit.",
+        image: [
+            "https://resizer.glanacion.com/resizer/yUrZ50cBrXLlseLAyfCKKuwpssA=/1200x746/smart/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/PAWJJLJ2IZC2DKEHKBWC2MIOYE.jpg"
+        ],
+        sales: 0,
+        categoryId: 1,
+        category: {
+            name: "Destilados",
+            id: 1
+        }
+    },
+    {
+        id:33,
+        name: "Whisky",
+        stock: 100,
+        cost: 10000,
+        description: "Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur aliquet quam id dui posuere blandit.",
+        image: [
+            "https://cursosonlineweb.com/wp-content/uploads/sites/51/2017/02/Clases-de-whisky.jpg"
+        ],
+        sales: 0,
+        categoryId: 1,
+        category: {
+            name: "Destilados",
+            id: 1
+        }
+    },
+    {
+        id:34,
+        name: "Cachaça",
+        stock: 100,
+        cost: 3500,
+        description: "Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur aliquet quam id dui posuere blandit.",
+        image: [
+            "https://static.educalingo.com/img/de/800/cachaca-1.jpg"
+        ],
+        sales: 0,
+        categoryId: 1,
+        category: {
+            name: "Destilados",
+            id: 1
+        }
+    }
+];
+
+module.exports = {
+    products,
+}
